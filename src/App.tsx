22c9f8bc381e7697bd11467bb3367f98acd17ff6
Anika van Rheede <anika.vanrheede@gmail.com>
2025-08-08 19:57:22 +0200

    Initial commit: Vite + React + TypeScript setup for ConVerse

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>ConVerse</h1>
+      <p>A Configurable Game of Life.</p>
+    </div>
+  );
+}
+
+export default App;
+
